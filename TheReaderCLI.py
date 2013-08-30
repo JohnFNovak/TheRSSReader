85073db2442921be8667e50c3463f63fa2d6d47c
JohntheBear <john.franc.novak@gmail.com>
2013-08-30 11:22:55 -0400

    added CLI interface code

---
+++ TheReaderCLI.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+# TheReaderCLI.py
+
+# John F. Novak (JFN)
+# Thursday, March 21, 2013, 9:22pm
+
+from TheReader import reader
+
+def Menu():
+
+
+if __name__ == '__main__':
+    global reader
+    reader = Reader()
+    Menu()
